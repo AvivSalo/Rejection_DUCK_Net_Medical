c5404931a9ccadbbc23734872831330db8ab5b5f
Aviv Salomon <121252358+AvivSalo@users.noreply.github.com>
2025-03-03 00:25:02 +0200

    Update ImageLoader2D.py

--- ImageLoader/ImageLoader2D.py
+++ ImageLoader/ImageLoader2D.py
@@ -1,33 +1,33 @@
 import glob
-
 import numpy as np
+import os
 from PIL import Image
 from skimage.io import imread
 from tqdm import tqdm
 
-folder_path = "/home/linuxu/PycharmProjects/Rejection_DUCK_Net_Medical/Data/Kvasir-SEG/"  # Add the path to your data directory
-
-
-def load_data(img_height, img_width, images_to_be_loaded, dataset):
-    IMAGES_PATH = folder_path + 'images/'
-    MASKS_PATH = folder_path + 'masks/'
+def load_data(img_height, img_width, images_to_be_loaded, dataset, data_path, split='train'):
+    IMAGES_PATH = os.path.join(data_path, split, 'images/')
+    MASKS_PATH = os.path.join(data_path, split, 'masks/')
 
     if dataset == 'kvasir':
         train_ids = glob.glob(IMAGES_PATH + "*.jpg")
-
-    if dataset == 'cvc-clinicdb':
+    elif dataset == 'cvc-clinicdb':
         train_ids = glob.glob(IMAGES_PATH + "*.tif")
-
-    if dataset == 'cvc-colondb' or dataset == 'etis-laribpolypdb':
+    elif dataset in ['cvc-colondb', 'etis-laribpolypdb']:
         train_ids = glob.glob(IMAGES_PATH + "*.png")
+    else:
+        raise ValueError(f"Unsupported dataset: {dataset}")
+
+    if not train_ids:
+        raise ValueError(f"No images found in {IMAGES_PATH}. Check the folder structure.")
 
     if images_to_be_loaded == -1:
         images_to_be_loaded = len(train_ids)
 
-    X_train = np.zeros((images_to_be_loaded, img_height, img_width, 3), dtype=np.float32)
-    Y_train = np.zeros((images_to_be_loaded, img_height, img_width), dtype=np.uint8)
+    X_data = np.zeros((images_to_be_loaded, img_height, img_width, 3), dtype=np.float32)
+    Y_data = np.zeros((images_to_be_loaded, img_height, img_width), dtype=np.uint8)
 
-    print('Resizing training images and masks: ' + str(images_to_be_loaded))
+    print(f'Resizing {split} images and masks: {images_to_be_loaded}')
     for n, id_ in tqdm(enumerate(train_ids)):
         if n == images_to_be_loaded:
             break
@@ -38,33 +38,25 @@ def load_data(img_height, img_width, images_to_be_loaded, dataset):
         image = imread(image_path)
         mask_ = imread(mask_path)
 
-        mask = np.zeros((img_height, img_width), dtype=np.bool_)
-
         pillow_image = Image.fromarray(image)
-
         pillow_image = pillow_image.resize((img_height, img_width))
         image = np.array(pillow_image)
 
-        X_train[n] = image / 255
+        X_data[n] = image / 255
 
         pillow_mask = Image.fromarray(mask_)
         pillow_mask = pillow_mask.resize((img_height, img_width), resample=Image.LANCZOS)
         mask_ = np.array(pillow_mask)
 
-        # Convert to binary (keeping 3 channels)
+        # Convert to binary (assuming mask_ is RGB or grayscale)
         binary_mask = (mask_ > 127).astype(np.uint8)
+        if len(binary_mask.shape) == 3:  # If RGB
+            binary_mask_single_channel = np.max(binary_mask, axis=-1)
+        else:  # If grayscale
+            binary_mask_single_channel = binary_mask
 
-        # Reduce to a single channel using max (any channel above 127 is set to 1)
-        binary_mask_single_channel = np.max(binary_mask, axis=-1)
+        Y_data[n] = binary_mask_single_channel
 
+    Y_data = np.expand_dims(Y_data, axis=-1)
 
-        """for i in range(img_height):
-            for j in range(img_width):
-                if mask_[i, j] >= 127:
-                    mask[i, j] = 1
-        """
-        Y_train[n] = binary_mask_single_channel
-
-    Y_train = np.expand_dims(Y_train, axis=-1)
-
-    return X_train, Y_train
+    return X_data, Y_data
